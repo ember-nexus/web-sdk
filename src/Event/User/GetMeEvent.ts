b1a14e325bc7ec4d49a1e60dc577bc9660d4a58d
Syndesi <soerenklein98@gmail.com>
2023-11-04 14:58:26 +0100

    wip

---
+++ src/Event/User/GetMeEvent.ts
@@ -0,0 +1,28 @@
+import type { Node } from '~/Type/Definition/Node';
+import { EventIdentifier } from '~/Type/Enum/EventIdentifierEnum';
+import { customEventDefaultInit } from '~/Type/Partial/CustomEventDefaultInit';
+
+type GetMeEventDetails = {
+  me: Promise<Node> | null;
+};
+
+class GetMeEvent extends CustomEvent<GetMeEventDetails> {
+  constructor() {
+    super(EventIdentifier.GetMe, {
+      ...customEventDefaultInit,
+      detail: {
+        me: null,
+      },
+    });
+  }
+
+  getElement(): Promise<Node> | null {
+    return this.detail.me;
+  }
+
+  setElement(me: Promise<Node>): void {
+    this.detail.me = me;
+  }
+}
+
+export { GetMeEvent, GetMeEventDetails };
---
+++ src/Event/User/PostRegisterEvent.ts
@@ -0,0 +1,43 @@
+import type { v4 as uuidv4 } from 'uuid';
+
+import { Data } from '~/Type/Definition/Data';
+import { EventIdentifier } from '~/Type/Enum/EventIdentifierEnum';
+import { customEventDefaultInit } from '~/Type/Partial/CustomEventDefaultInit';
+
+type PostRegisterEventDetails = {
+  password: string;
+  data: Data;
+  result: Promise<uuidv4> | null;
+};
+
+class PostRegisterEvent extends CustomEvent<PostRegisterEventDetails> {
+  constructor(password: string, data: Data = {}) {
+    super(EventIdentifier.PostRegister, {
+      ...customEventDefaultInit,
+      detail: {
+        password: password,
+        data: data,
+        result: null,
+      },
+    });
+  }
+
+  getPassword(): string {
+    return this.detail.password;
+  }
+
+  getData(): Data {
+    return this.detail.data;
+  }
+
+  getResult(): Promise<uuidv4> | null {
+    return this.detail.result;
+  }
+
+  setResult(result: Promise<uuidv4> | null): PostRegisterEvent {
+    this.detail.result = result;
+    return this;
+  }
+}
+
+export { PostRegisterEvent, PostRegisterEventDetails };
