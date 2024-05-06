002323e31317e4cbb4c169129d0d413622fb9bb6
Syndesi <soerenklein98@gmail.com>
2024-05-06 08:23:58 +0200

    Add DeleteTokenEvent.

---
+++ src/BrowserEvent/User/DeleteTokenEvent.ts
@@ -0,0 +1,29 @@
+import { EventIdentifier } from '~/Type/Enum/EventIdentifier';
+import { customEventDefaultInit } from '~/Type/Partial/CustomEventDefaultInit';
+
+type DeleteTokenEventDetails = {
+  result: Promise<void> | null;
+};
+
+class DeleteTokenEvent extends CustomEvent<DeleteTokenEventDetails> {
+  public static type = EventIdentifier.DeleteToken;
+  constructor() {
+    super(DeleteTokenEvent.type, {
+      ...customEventDefaultInit,
+      detail: {
+        result: null,
+      },
+    });
+  }
+
+  getResult(): Promise<void> | null {
+    return this.detail.result;
+  }
+
+  setResult(result: Promise<void> | null): DeleteTokenEvent {
+    this.detail.result = result;
+    return this;
+  }
+}
+
+export { DeleteTokenEvent, DeleteTokenEventDetails };
